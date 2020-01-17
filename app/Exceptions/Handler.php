cd52813252cd2c5b51264f568012bb08f1103d96
Duncan I. Mwangi <irungu.mwangi2@gmail.com>
2020-01-17 08:34:57 +0300

    wip

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,7 +46,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($e instanceof \Illuminate\Session\TokenMismatchException) {
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
             
             return redirect()->route('login');
  
