bedcab2f2ce068c5abf999ccb4917b9230d93e6c
Duncan I. Mwangi <irungu.mwangi2@gmail.com>
2020-02-14 10:44:35 +0300

    wip

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -18,6 +18,11 @@
     export default {
         mounted() {
             console.log('Component mounted.')
+        },
+        data: function () {
+            return {
+
+            }
         }
     }
 </script>
